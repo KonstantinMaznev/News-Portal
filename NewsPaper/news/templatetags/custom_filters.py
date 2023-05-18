6a451d065b81ecac969feca301c9d43207638abe
KonstantinMaznev <knmaznev@icloud.com>
2023-05-18 20:02:15 +0300

    fix views in news and add filter

---
+++ NewsPaper/news/templatetags/custom_filters.py
@@ -0,0 +1,22 @@
+from django import template
+
+
+register = template.Library()
+
+BAD_WORDS = [
+    'Мышь','мышь','глупец','Глупец','плохишь','Плохишь'
+]
+
+
+title = 'post.article_title'
+text = 'post.text'
+
+@register.filter()
+def censor(text):
+    list = text.split()
+    censor_list = []
+    for word in list:
+        t1 = []
+        if t1 in BAD_WORDS:
+
+
